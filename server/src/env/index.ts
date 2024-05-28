ba41c26dea43c057fde2ac55240ce323cfece9ed
Pedro Mendes <pedromendes@claretiano.edu.br>
2024-05-28 10:44:02 -0300

    chore: add folder structure

---
+++ server/src/env/index.ts
@@ -0,0 +1,16 @@
+import { InternalServerError } from '@/core/errors/internal-server-error'
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables:', _env.error.format())
+  throw InternalServerError('Invalid environment variables')
+}
+
+export const env = _env.data
