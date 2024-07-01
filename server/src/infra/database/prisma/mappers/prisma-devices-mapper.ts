44991f6093458c1ada1b4b7e24aa048b49270242
Pedro Henrique <pedrohenriquecamposmendes@hotmail.com>
2024-06-30 22:10:18 -0300

    feat: implements device mapper and repository

---
+++ server/src/infra/database/prisma/mappers/prisma-devices-mapper.ts
@@ -0,0 +1,38 @@
+import { Device as PrismaDevice, Prisma } from "@prisma/client"
+
+import { Device, DeviceProps } from "@/domain/it-manager/enterprise/entities/device"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Slug } from "@/domain/it-manager/enterprise/entities/value-objects/slug"
+
+export class PrismaDevicesMapper {
+  static toDomain(raw: PrismaDevice): Device<DeviceProps> {
+    const id = new UniqueEntityID(raw.id)
+    const modelSlug = Slug.createFromText(raw.model)
+
+    return Device.create(
+      {
+        serialNumber: raw.serialNumber,
+        model: raw.model,
+        invoiceNumber: raw.invoiceNumber,
+        acquisitionDate: raw.acquisitionDate,
+        modelSlug,
+        contractId: raw.contractId,
+        endWarrantyDate: raw.endWarrantyDate,
+      },
+      id,
+    )
+  }
+
+  static toPersistence(device: Device<DeviceProps>): Prisma.DeviceUncheckedCreateInput {
+    return {
+      id: device.id.toString(),
+      serialNumber: device.serialNumber,
+      model: device.model,
+      invoiceNumber: device.invoiceNumber,
+      acquisitionDate: device.acquisitionDate,
+      modelSlug: device.modelSlug,
+      contractId: device.contractId,
+      endWarrantyDate: device.endWarrantyDate,
+    }
+  }
+}
--- server/src/infra/database/prisma/repositories/prisma-devices-repository.ts
+++ server/src/infra/database/prisma/repositories/prisma-devices-repository.ts
@@ -2,39 +2,73 @@ import { PrismaClient } from '@prisma/client'
 
 import { DeviceRepository } from '@/domain/it-manager/application/repositories/device-repository'
 import { Device, DeviceProps } from '@/domain/it-manager/enterprise/entities/device'
+import { PrismaDevicesMapper } from '../mappers/prisma-devices-mapper'
 
 export class PrismaDevicesRepository implements DeviceRepository {
   constructor(private prisma: PrismaClient) {}
 
-  findMany(): Promise<Device<DeviceProps>[]> {
-    throw new Error('Method not implemented.')
+  async findMany(): Promise<Device<DeviceProps>[]> {
+    const devices = await this.prisma.device.findMany()
+
+    return devices.map(PrismaDevicesMapper.toDomain)
   }
 
-  findById(id: string): Promise<Device<DeviceProps> | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Device<DeviceProps> | null> {
+    const device = await this.prisma.device.findUnique({
+      where: { id },
+    })
+
+    if (!device) {
+      return null
+    }
+
+    return PrismaDevicesMapper.toDomain(device)
   }
 
-  findBySerialNumber(serialNumber: string): Promise<Device<DeviceProps> | null> {
-    throw new Error('Method not implemented.')
+  async findBySerialNumber(serialNumber: string): Promise<Device<DeviceProps> | null> {
+    const device = await this.prisma.device.findUnique({
+      where: { serialNumber },
+    })
+
+    if (!device) {
+      return null
+    }
+
+    return PrismaDevicesMapper.toDomain(device)
   }
 
-  findByModel(model: string): Promise<Device<DeviceProps>[]> {
-    throw new Error('Method not implemented.')
+  async findByModel(model: string): Promise<Device<DeviceProps>[]> {
+    const devices = await this.prisma.device.findMany({
+      where: { model },
+    })
+
+    return devices.map(PrismaDevicesMapper.toDomain)
   }
 
-  findByInvoiceNumber(invoiceNumber: string): Promise<Device<DeviceProps>[]> {
-    throw new Error('Method not implemented.')
+  async findByInvoiceNumber(invoiceNumber: string): Promise<Device<DeviceProps>[]> {
+    const devices = await this.prisma.device.findMany({
+      where: { invoiceNumber },
+    })
+
+    return devices.map(PrismaDevicesMapper.toDomain)
   }
 
-  create(device: Device<DeviceProps>): Promise<void> {
-    throw new Error('Method not implemented.')
+  async create(device: Device<DeviceProps>): Promise<void> {
+    const data = PrismaDevicesMapper.toPersistence(device)
+
+    await this.prisma.device.create({ data })
   }
 
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(id: string): Promise<void> {
+    await this.prisma.device.delete({ where: { id } })
   }
 
-  save(device: Device<DeviceProps>): Promise<void> {
-    throw new Error('Method not implemented.')
+  async save(device: Device<DeviceProps>): Promise<void> {
+    const data = PrismaDevicesMapper.toPersistence(device)
+
+    await this.prisma.device.update({
+      where: { id: data.id },
+      data,
+    })
   }
 }
