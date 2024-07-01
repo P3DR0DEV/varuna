70f069660120f7ec033960dad7fb358f748245b1
Pedro Henrique <pedrohenriquecamposmendes@hotmail.com>
2024-06-30 21:28:30 -0300

    feat: create prisma service and mapper

--- server/src/infra/database/prisma/repositories/prisma-services-repository.ts
+++ server/src/infra/database/prisma/repositories/prisma-services-repository.ts
@@ -2,35 +2,63 @@ import { PrismaClient } from '@prisma/client'
 
 import { ServiceRepository } from '@/domain/it-manager/application/repositories/service-repository'
 import { Service, ServiceTypes } from '@/domain/it-manager/enterprise/entities/service'
+import { MapServiceType, PrismaServiceMapper } from '../mappers/prisma-services-mapper'
 
 export class PrismaServicesRepository implements ServiceRepository {
   constructor(private prisma: PrismaClient) {}
 
-  findMany(): Promise<Service[]> {
-    throw new Error('Method not implemented.')
+  async findMany(): Promise<Service[]> {
+    const services = await this.prisma.service.findMany()
+
+    return services.map(PrismaServiceMapper.toDomain)
   }
 
-  findById(id: string): Promise<Service | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Service | null> {
+    const service = await this.prisma.service.findUnique({
+      where: { id },
+    })
+
+    if (!service) {
+      return null
+    }
+
+    return PrismaServiceMapper.toDomain(service)
   }
 
-  findByType(type: ServiceTypes): Promise<Service[]> {
-    throw new Error('Method not implemented.')
+  async findByType(type: ServiceTypes): Promise<Service[]> {
+    const persistenceType = MapServiceType.toPersistence(type)
+
+    const services = await this.prisma.service.findMany({
+      where: { type: persistenceType },
+    })
+
+    return services.map(PrismaServiceMapper.toDomain)
   }
 
-  findByIpAddress(ipAddress: string): Promise<Service[]> {
-    throw new Error('Method not implemented.')
+  async findByIpAddress(ipAddress: string): Promise<Service[]> {
+    const services = await this.prisma.service.findMany({
+      where: { ipAddress },
+    })
+
+    return services.map(PrismaServiceMapper.toDomain)
   }
 
-  create(service: Service): Promise<void> {
-    throw new Error('Method not implemented.')
+  async create(service: Service): Promise<void> {
+    const data = PrismaServiceMapper.toPersistence(service)
+
+    await this.prisma.service.create({ data })
   }
 
-  save(service: Service): Promise<void> {
-    throw new Error('Method not implemented.')
+  async save(service: Service): Promise<void> {
+    const data = PrismaServiceMapper.toPersistence(service)
+
+    await this.prisma.service.update({
+      where: { id: data.id },
+      data,
+    })
   }
 
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(id: string): Promise<void> {
+    await this.prisma.service.delete({ where: { id } })
   }
 }
