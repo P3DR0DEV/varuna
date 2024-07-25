37f37c8e4162d04bc6658e3891cd9f7fd129b247
Pedro Mendes <pedromendes@claretiano.edu.br>
2024-07-25 15:26:09 -0300

    fix: update e2e config
    add dotenv package
    remove query log from prisma
    update server initialization

--- server/test/setup-e2e.ts
+++ server/test/setup-e2e.ts
@@ -1,27 +1,28 @@
+import 'dotenv/config'
+
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
 
 import { PrismaClient } from '@prisma/client'
 
-import { env } from '@/env'
-
 const prisma = new PrismaClient()
 
-function generateUniqueDatabaseUrl(schemaId: string) {
-  const url = new URL(env.DATABASE_URL)
+function generateDatabaseUrl(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please set DATABASE_URL env variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
   url.searchParams.set('schema', schemaId)
 
   return url.toString()
 }
 
 const schemaId = randomUUID()
+const databaseUrl = generateDatabaseUrl(schemaId)
+process.env.DATABASE_URL = databaseUrl
 
-beforeAll(async () => {
-  const databaseUrl = generateUniqueDatabaseUrl(schemaId)
-  env.DATABASE_URL = databaseUrl
-
-  execSync('pnpx prisma migrate deploy')
-})
+execSync('pnpx prisma migrate deploy')
 
 afterAll(async () => {
   await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
