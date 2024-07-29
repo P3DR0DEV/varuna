54980f48102826f78fed2206bffd0fed884e5cf6
Pedro Mendes <pedromendes@claretiano.edu.br>
2024-07-29 10:43:25 -0300

    fix: remove wrong return errors

--- server/src/infra/http/controllers/service/edit-service.ts
+++ server/src/infra/http/controllers/service/edit-service.ts
@@ -27,14 +27,6 @@ export async function editService(app: FastifyInstance) {
           200: z.object({
             service: serviceSchema,
           }),
-          400: z.object({
-            name: z.string(),
-            message: z.string(),
-          }),
-          404: z.object({
-            name: z.string(),
-            message: z.string(),
-          }),
         },
       },
     },
--- server/src/infra/http/controllers/service/fetch-services-by-ip-address.ts
+++ server/src/infra/http/controllers/service/fetch-services-by-ip-address.ts
@@ -20,14 +20,6 @@ export async function fetchServicesByIpAddress(app: FastifyInstance) {
           200: z.object({
             services: z.array(serviceSchema),
           }),
-          400: z.object({
-            name: z.string(),
-            message: z.string(),
-          }),
-          404: z.object({
-            name: z.string(),
-            message: z.string(),
-          }),
         },
       },
     },
